030e3e70eb66941d16729f4a4527fe3203d3c8f5
Russell <seanvilleza40@gmail.com>
2026-01-11 13:43:28 +0800

    s

--- build/lib/orthobasis/exceptions.py
+++
@@ -1,7 +0,0 @@
-class InvalidVectorSetError(Exception):
-    """Raised when a vector set is invalid (e.g., contains zero vectors)"""
-    pass
-
-class DependentVectorSetError(Exception):
-    """Raised when vectors are linearly dependent"""
-    pass
